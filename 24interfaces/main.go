641a137719cef30f1c6817485f0dc6b44d547709
sai <saivaraprasad072@gmail.com>
2026-06-09 07:04:29 +0530

    done with basics on interfaces

--- 24interfaces/main.go
+++ 24interfaces/main.go
@@ -26,10 +26,28 @@ func (c Circle) Area() float64 {
 	return math.Pi * float64(c.Radius) * float64(c.Radius)
 }
 
+func Check(v interface{}) {
+    switch x := v.(type) {
+
+    case int:
+        fmt.Println("Integer", x)
+
+    case string:
+        fmt.Println("String", x)
+
+    case bool:
+        fmt.Println("Boolean", x)
+
+    default:
+        fmt.Println("Unknown")
+    }
+}
+
 func main() {
 	rect := Rectangle{Length: 5, Width: 3}
 	circle := Circle{Radius: 2}
 
-	fmt.Printf("Rectangle area: %.5f\n", rect.Area())
-	fmt.Printf("Circle area: %.5f\n", circle.Area())
+	fmt.Printf("Rectangle area: %.2f\n", rect.Area())
+	fmt.Printf("Circle area: %.2f\n", circle.Area())
+	Check(69)
 }
